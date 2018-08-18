7a18390d0aca6af5147f82b88abbf0ee27798659
WeiChengHung <s777610@gmail.com>
2018-08-18 02:18:07 -0700

    add home page

--- src/app.py
+++ src/app.py
@@ -16,9 +16,15 @@ def plot_security(name, ticker, daily_return, cum_return):
                            last_updated=last_updated, daily_return=daily_return, cum_return=cum_return,
                            div1=div1, div2=div2, div3=div3)
 
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 @app.route('/etf/')
@@ -55,5 +61,5 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4880)
+    app.run(debug=True, port=4881)
 
