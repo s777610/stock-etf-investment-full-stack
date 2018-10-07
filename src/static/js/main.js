170c20e2442f735c346fd5f415714c6a59e7b4f9
s777610 <s777610@gmail.com>
2018-10-06 21:18:13 -0700

    js external file

---
+++ src/static/js/main.js
@@ -0,0 +1,54 @@
+
+// layout.html
+$(".draggable").draggable();
+
+
+// home.html
+$(document).ready(function() {
+  $(function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      modal: true
+    });
+  });
+
+  $("#email-button").click(function(){s
+    $( "#dialog" ).dialog( "open" );
+  })
+})
+
+
+// securities_list.html
+$( function() {
+  var state = true;
+  $( "#button" ).on( "click", function() {
+    if ( state ) {
+      $( "#effect" ).animate({
+        backgroundColor: "#5E5E5E",
+        color: "#fff",
+        width: 900,
+        height: 150
+      }, 1000 );
+    } else {
+      $( "#effect" ).animate({
+        backgroundColor: "#fff",
+        color: "#000",
+        width: 900,
+        height: 100
+      }, 1000 );
+    }
+    state = !state;
+  });
+} );
+
+$( function() {
+    var state = true;
+    $("#button").on("click", function () {
+      if ( state ) {
+          $("#effect").addClass("newClass");
+      } else {
+          $( "#effect" ).removeClass( "newClass" );
+      }
+      state = !state;
+    });
+});
