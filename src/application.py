edc67aa8438fa4fbafa430fa80528417685ea0da
s777610 <s777610@gmail.com>
2018-10-22 22:22:14 -0700

    add shortcut icon

--- src/application.py
+++ src/application.py
@@ -3,7 +3,6 @@ from models.plot import Plot
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'mysecretkey'
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -11,23 +10,26 @@ application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(application)
 
 
-
 @application.route('/')
 def home():
     return render_template("home.html")
+
 
 @application.route('/plot/<string:ticker>/<string:name>/<string:daily_return>/<string:cum_return>')
 def plot_security(name, ticker, daily_return, cum_return):
     plot = Plot(ticker, name)
     return render_template("plot_stock.html", plot=plot, daily_return=daily_return, cum_return=cum_return)
 
+
 @application.route('/resume')
 def resume():
     return render_template("resume.html")
 
+
 @application.route('/about')
 def about():
     return render_template("about.html")
+
 
 @application.route('/securitieslist/<string:type>')
 def securitieslist(type):
