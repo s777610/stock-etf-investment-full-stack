3318f205c396600d4984b4f624e452e159317cef
s777610 <s777610@gmail.com>
2018-10-24 15:24:15 -0700

    fix footer

--- src/common/name_scraper.py
+++ src/common/name_scraper.py
@@ -4,12 +4,18 @@ import requests
 
 def scraper(ticker):
     try:
-        url = f"https://www.marketwatch.com/investing/stock/{ticker}"
+        #https://www.barchart.com/stocks/quotes/vig
+        #url = f"https://www.marketwatch.com/investing/stock/{ticker}"
+        url = f"https://www.barchart.com/stocks/quotes/{ticker}"
         request = requests.get(url)
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
         # <h1 class="company__name">MongoDB Inc.</h1>
-        security_name = soup.find("h1", {"class": "company__name"}).text
+        security_name = soup.find("span", {"class": "symbol"}).text
         return security_name
     except:
         return None
+
+
+name = scraper("vig")
+print(name)
