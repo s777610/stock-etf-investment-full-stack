6197291efc0a59dfdbdbf55f335637bfa58c6e9b
s777610 <s777610@gmail.com>
2019-01-31 19:14:02 -0800

    add metadata

--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -1,21 +1,20 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
-    entry: ["@babel/polyfill", "./js/main.js"],
-    output: {
-        path: path.resolve(__dirname, 'js'),
-        filename: 'main.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+  entry: ["@babel/polyfill", "./js/main.js"],
+  output: {
+    path: path.resolve(__dirname, "js"),
+    filename: "main.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 };
